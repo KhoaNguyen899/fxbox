446736c31855105f8a102321cdb8954b3d347164
Nguyen Minh Khoa <khoa.nguyen@icetea.io>
2023-12-18 15:22:00 +0700

    update

--- iCard.sol
+++ iCard.sol
@@ -1,67 +1,36 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "./ICard.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract FxBox is Ownable(msg.sender), ReentrancyGuard {
-    using SafeERC20 for IERC20;
+interface ICard {
+    /**
+     * @dev Upgrades the card level
+     * @param tokenId The token ID to upgrade
+     * @param level The new level
+     */
+    function upgradeCardLevel(uint256 tokenId, uint256 level) external;
 
-    IERC20 public currencyToken;
-    uint256 public price;
-    address public recipient;
-    ICard public fxCard;
-    uint256 public supply;
+    /**
+     * @dev Mints a new token
+     * @param to The address to mint the token to
+     */
+    function safeMint(address to) external;
 
-    constructor(
-        address _fxCard,
-        address _currencyToken,
-        uint256 _price,
-        uint256 _supply,
-        address _recipient
-    ) {
-        currencyToken = IERC20(_currencyToken);
-        price = _price;
-        fxCard = ICard(_fxCard);
-        recipient = _recipient;
-        supply = _supply;
-    }
+    /**
+     * @dev Upgrades the card opened status
+     * @param nftId The token ID to upgrade
+     * @param nftOwner The nft owner
+     */
+    function stakeNFT(address nftOwner, uint256 nftId) external;
 
-    function setPrice(uint256 _price) external onlyOwner {
-        price = _price;
-    }
-
-    function setRecipient(address _recipient) external onlyOwner {
-        recipient = _recipient;
-    }
-
-    function setCurrencyToken(address _currencyToken) external onlyOwner {
-        currencyToken = IERC20(_currencyToken);
-    }
-
-    function setSupply(uint256 _supply) external onlyOwner {
-        supply = _supply;
-    }
-
-    function buyBox(uint256 _amount) external nonReentrant {
-        require(_amount > 0, "Amount should be greater than 0");
-        require(supply >= _amount, "Insufficient supply");
-        require(msg.sender == tx.origin, "Contracts are not allowed");
-
-        currencyToken.safeTransferFrom(msg.sender, recipient, _amount * price);
-
-        for (uint256 i = 0; i < _amount; i++) {
-            fxCard.safeMint(msg.sender);
-        }
-
-        supply -= _amount;
-
-        emit BuyedBox(msg.sender, _amount, _amount * price);
-    }
-
-    /// @notice event emitted when a user has Deposited a LP
-    event BuyedBox(address owner, uint256 amount, uint256 totalPrice);
+    /**
+     * @dev Upgrades the card opened status
+     * @param owner The token ID to upgrade
+     * @param tokenID The nft owner
+     */
+    function mintCard(
+        address owner,
+        uint256 tokenID
+    ) external returns (uint256);
 }
